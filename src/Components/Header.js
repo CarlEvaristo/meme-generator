0ad6c2fa94afe042147bfc9c412e7267eb4418e0
CarlEverest78 <thepythoneer@gmail.com>
2022-08-13 14:39:24 +0200

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from  "react"
+
+export default function Header() {
+    return(
+        <header>
+            <img src="/images/meme-face.png" alt="logo" />
+            <h1>Meme Generator</h1>
+            <div className="subtitle">React Course - Project 3</div>
+        </header>
+    )
+}
