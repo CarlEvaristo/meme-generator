1707da8ed033bfe0ab002dcab676fc0f99cb4abc
CarlEverest78 <thepythoneer@gmail.com>
2022-08-13 17:17:34 +0200

    final commit (without useState)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <header>
             <img src="/images/meme-face.png" alt="logo" />
             <h1>Meme Generator</h1>
-            <div className="subtitle">React Course - Project 3</div>
+            <div className="subtitle">React Project 3</div>
         </header>
     )
 }
