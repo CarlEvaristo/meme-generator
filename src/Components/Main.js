9dbfeeb8266653d02610a428ed1a2450a8a95046
CarlEverest78 <thepythoneer@gmail.com>
2022-08-15 13:04:10 +0200

    turn state into object

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,29 +1,43 @@
 import React from "react"
 
-function changeHandler(){
-    const input1 = document.getElementById("input-1").value
-    const input2 = document.getElementById("input-2").value
 
-    document.getElementById("topText").textContent = input1
-    document.getElementById("bottomText").textContent = input2
-}
-
-function buttonHandler(e){
-    e.preventDefault();
-
-
-    fetch("https://api.imgflip.com/get_memes")
-        .then(response => response.json())
-        .then(data => {
-            let memesList = data.data.memes
-            let randomIndex = Math.floor((Math.random() * 100) + 1)   // Returns a random integer from 0 to 100:
-            let randomMemeImgUrl = memesList[randomIndex].url
-            document.getElementById("memeImg").src = randomMemeImgUrl
-        })
-}
 
 function Main() {
-    window.onload = buttonHandler
+    const [meme, setMeme] = React.useState({
+        image: "https://i.imgflip.com/1ii4oc.jpg",
+        topText: "I love this autograph",
+        bottomText: "It's great, it's the best"
+    })
+
+    function changeHandler(){
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                topText: document.getElementById("input-1").value,
+                bottomText: document.getElementById("input-2").value
+            }
+        })
+    }
+    
+    function getMemeImage(e){
+        e.preventDefault()
+        
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data => {
+                let memesList = data.data.memes
+                let randomIndex = Math.floor((Math.random() * 100) + 1)   // Returns a random integer from 0 to 100:
+                let randomMemeImgUrl = memesList[randomIndex].url
+                
+                setMeme(prevMeme => {
+                    return {
+                        ...prevMeme,
+                        image: randomMemeImgUrl
+                    }
+                })
+            })
+    }
+
     return(
         <main>
             <form method="GET" id="myForm" >
@@ -31,12 +45,12 @@ function Main() {
                     <input type="text" placeholder="Line 1" id="input-1" onChange={changeHandler}></input>
                     <input type="text" placeholder="Line 2" id="input-2" onChange={changeHandler}></input>
                 </div>
-                <button type="submit" onClick={buttonHandler}>Get a new meme image 🖼</button>
+                <button type="submit" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </form>
             <div className="imgContainer">
-                <img id="memeImg" alt="meme" />
-                <p id="topText"></p>
-                <p id="bottomText"></p>
+                <img src={meme.image} id="memeImg" alt="meme" />
+                <p id="topText">{meme.topText}</p>
+                <p id="bottomText">{meme.bottomText}</p>
             </div>
 
         </main>
