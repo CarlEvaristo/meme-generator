b30fdb53fda7f96d5488e6271dc6e2453d4fefd7
CarlEverest78 <thepythoneer@gmail.com>
2022-08-19 13:32:23 +0200

    add fontsize state/input

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,54 +3,57 @@ import React from "react"
 
 
 function Main() {
+    const [allMemes, setAllMemes] = React.useState()
     const [meme, setMeme] = React.useState({
         image: "https://i.imgflip.com/1ii4oc.jpg",
         topText: "I love this autograph",
-        bottomText: "It's great, it's the best"
+        bottomText: "It's great, it's the best",
+        fontSize: 33
     })
 
-    function changeHandler(){
+    function changeHandler(event){
+        const {name, value} = event.target
         setMeme(prevMeme => {
             return {
                 ...prevMeme,
-                topText: document.getElementById("input-1").value,
-                bottomText: document.getElementById("input-2").value
+                [name]: value
             }
         })
     }
-    
-    function getMemeImage(e){
-        e.preventDefault()
-        
+
+    React.useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
-            .then(data => {
-                let memesList = data.data.memes
-                let randomIndex = Math.floor((Math.random() * 100) + 1)   // Returns a random integer from 0 to 100:
-                let randomMemeImgUrl = memesList[randomIndex].url
-                
-                setMeme(prevMeme => {
-                    return {
-                        ...prevMeme,
-                        image: randomMemeImgUrl
-                    }
-                })
-            })
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getMemeImage(e){
+        e.preventDefault()
+        let randomIndex = Math.floor((Math.random() * 100) + 1)   // Returns a random integer from 0 to 100:
+        let randomMemeImgUrl = allMemes[randomIndex].url
+        
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                image: randomMemeImgUrl
+            }
+        })  
     }
 
     return(
         <main>
             <form method="GET" id="myForm" >
                 <div className="form">
-                    <input type="text" placeholder="Line 1" id="input-1" onChange={changeHandler}></input>
-                    <input type="text" placeholder="Line 2" id="input-2" onChange={changeHandler}></input>
+                    <input type="text" placeholder="Line 1" name="topText" onChange={changeHandler} value={meme.topText} />
+                    <input type="text" placeholder="Line 2" name="bottomText" onChange={changeHandler} value={meme.bottomText} />
+                    <input type="number" id="fontsize" name="fontSize" min="1" max="100" value={meme.fontSize} onChange={changeHandler}></input>
                 </div>
                 <button type="submit" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </form>
             <div className="imgContainer">
                 <img src={meme.image} id="memeImg" alt="meme" />
-                <p id="topText">{meme.topText}</p>
-                <p id="bottomText">{meme.bottomText}</p>
+                <p id="topText" style={{fontSize:Number(meme.fontSize)}}>{meme.topText}</p>
+                <p id="bottomText" style={{fontSize:Number(meme.fontSize)}}>{meme.bottomText}</p>
             </div>
 
         </main>
